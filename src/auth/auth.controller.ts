ebb0c00f31a29a6c19d28c788ccadf803d4c3c4d
Yesid Llamas <yellaber@gmail.com>
2024-10-22 10:50:17 -0500

    auth.controller.ts modificado

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,34 +1,58 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Request } from '@nestjs/common';
 
-@Controller('auth')
+import { AuthService } from './auth.service';
+import { AuthGuard } from './guards/auth.guard';
+import { CreateUserDto, LoginDto, RegisterUserDto, UpdateUserDto } from './dto';
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
+
+@Controller( 'auth' )
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor( private readonly authService: AuthService ) {}
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  create( @Body() createUserDto: CreateUserDto ) {
+    return this.authService.create( createUserDto );
   }
 
+  @Post( '/login' )
+  login(@Body() loginDto: LoginDto ) {
+    return this.authService.login( loginDto );
+  }
+
+  @Post( '/register' )
+  register( @Body() registerUserDto: RegisterUserDto ) {
+    return this.authService.register( registerUserDto );
+  }
+
+  @UseGuards( AuthGuard )
   @Get()
   findAll() {
     return this.authService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
+  @UseGuards( AuthGuard )
+  @Get( 'check-token' )
+  checkToken( @Request() request: Request ): LoginResponse {
+    const user = request[ 'user' ] as User;
+    return {
+      user,
+      token: this.authService.getJWT({ id: user._id })
+    };
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
+  /*@Get( ':id' )
+  findOne( @Param( 'id' ) id: string ) {
+    return this.authService.findOne( +id );
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Patch( ':id' )
+  update( @Param( 'id' ) id: string, @Body() updateUserDto: UpdateUserDto ) {
+    return this.authService.update( +id, updateUserDto );
   }
+
+  @Delete( ':id' )
+  remove( @Param( 'id' ) id: string ) {
+    return this.authService.remove( +id );
+  }*/
 }
